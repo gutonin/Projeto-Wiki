a0acc5dfbd15f16182cd804ca15ce2b3c8969955
gutonin <gutonin@gmail.com>
2020-12-19 12:01:26 -0300

    adicao de categoria na view materias | Adicionado categoria no template | form de materia

--- wiki/materias/views.py
+++ wiki/materias/views.py
@@ -4,11 +4,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Materia, Categoria
 
 def listagem(request):
-    materias = Materia.objects.all().order_by('-data_criacao')
+    # materias = Materia.objects.all().order_by('-data_criacao')
+    materias = Materia.objects.filter(categoria__id=1).order_by('-data_criacao')
+    categorias = Categoria.objects.all()
     context={
         'titulo':'Minha Wiki',
         'titulo_pagina':'Materias',
         'materias':materias,
+        'chave':categorias,
     }
     return render(request, 'listagem.html', context)
 
